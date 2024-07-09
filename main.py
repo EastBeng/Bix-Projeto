0f044fc193f045faca1c6960f3a7b1d3d8ca7a82
Gabriel De Souza Passos <44300759+EastBeng@users.noreply.github.com>
2024-07-09 19:27:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -78,6 +78,11 @@ print("Matriz de Confusão:\n", conf_matrix)
 print("Relatório de Classificação:\n", class_report)
 
 # Treinar o modelo de Random Forest com GridSearchCV
+#param_grid = {'n_estimators': [100, 200], 'max_depth': [None, 10, 20], 'min_samples_split': [2, 5, 10]}
+#rf_clf = RandomForestClassifier(random_state=42)
+#grid_search = GridSearchCV(rf_clf, param_grid, cv=5, scoring='accuracy')
+#grid_search.fit(X_train, y_train)
+
 param_grid = {
     'n_estimators': [100, 200], 
     'max_depth': [None, 10], 
